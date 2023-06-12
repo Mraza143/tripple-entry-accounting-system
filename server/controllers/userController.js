981fb1c9b234563a9e318e000f0745a78aed3415
Raza <mraza6601@gmail.com>
2023-06-12 05:40:40 +0500

    update user api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -128,3 +128,20 @@ export const removeUser = async (req, res, next) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+
+export const updateUserRole = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const user = await UserModal.findById(userId);
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'User not found' });
+    }
+    user.role = 'admin';
+    const updatedUser = await user.save();
+    return res.status(200).json({ success: true, user: updatedUser });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,11 +6,13 @@ import {
   loginUser,
   getAllUsers,
   removeUser,
+  updateUserRole
 } from "../controllers/userController.js";
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/allUsers").get(getAllUsers);
 router.route("/deleteUser/:id").delete(removeUser);
+router.route("/updateRole/:id").put(updateUserRole);
 
 export default router;
